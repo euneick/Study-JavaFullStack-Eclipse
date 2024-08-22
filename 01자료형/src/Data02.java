d6c2c8d8f6f3a62b3444cfdd7c49efb812cd109f
euneick <study.euneick@gmail.com>
2024-08-22 10:14:59 +0900

    더 다양한 종류의 상수들 출력

---
+++ 01자료형/src/Data02.java
@@ -0,0 +1,14 @@
+
+/**
+ * 더 다양한 종류의 상수들 출력
+ */
+public class Data02 {
+
+	public static void main(String[] args) {
+
+		System.out.println(1l);
+		System.out.println(1.5f);
+		System.out.println("abc");
+	}
+
+}
