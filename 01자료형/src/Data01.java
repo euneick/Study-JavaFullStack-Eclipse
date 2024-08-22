25a93997702881cd6356886fde1fbc46f757869e
euneick <study.euneick@gmail.com>
2024-08-22 10:06:13 +0900

    자바 문법에서 사용되는 상수의 종류 출력

---
+++ 01자료형/src/Data01.java
@@ -0,0 +1,15 @@
+
+/**
+ * 자바 문법에서 사용되는 상수의 종류 출력
+ */
+public class Data01 {
+
+	public static void main(String[] args) {
+
+		System.out.println(1);
+		System.out.println(1.5);
+		System.out.println('a');
+		System.out.println(true);
+	}
+
+}
