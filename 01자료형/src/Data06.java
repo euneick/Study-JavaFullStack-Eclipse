00f21a62c9a52cd8c9e565c2e70c42c4c48d1056
euneick <study.euneick@gmail.com>
2024-08-22 13:08:46 +0900

    실수 형태의 자료형 저장하기

---
+++ 01자료형/src/Data06.java
@@ -0,0 +1,16 @@
+
+/**
+ * 실수 형태의 자료형 저장하기
+ */
+public class Data06 {
+
+	public static void main(String[] args) {
+		
+		float f = 23.7f;		// 숫자 뒤에 f 를 붙이지 않으면 double 자료형으로 인식되어 type mismatch 가 발생
+		double d = 23.7;
+		
+		System.out.println(f);
+		System.out.println(d);
+	}
+
+}
