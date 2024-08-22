a0f61e788749883ba49a95b3c361f2700849c599
euneick <study.euneick@gmail.com>
2024-08-22 16:11:06 +0900

    문자열 데이터에 + 연산하기

---
+++ 02연산자/src/Opr01_01.java
@@ -0,0 +1,15 @@
+
+/**
+ * 문자열 데이터에 + 연산하기
+ */
+public class Opr01_01 {
+
+	public static void main(String[] args) {
+		
+		String str1 = "hello ";
+		String str2 = "world!";
+		
+		System.out.println(str1 + str2);
+	}
+
+}
