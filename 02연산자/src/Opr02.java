2fe1607d7745abab981b4841258269c101101fb3
euneick <study.euneick@gmail.com>
2024-08-22 17:18:06 +0900

    수치 데이터의 대소 관계 살피기

---
+++ 02연산자/src/Opr02.java
@@ -0,0 +1,18 @@
+
+/**
+ * 수치 데이터의 대소 관계 살피기
+ */
+public class Opr02 {
+
+	public static void main(String[] args) {
+
+		int a = 10, b = 4, c;
+		boolean test;
+
+		test = a > b;
+		System.out.println(a + " > " + b + " = " + test);
+
+		System.out.println((a + b) > (a - b));
+	}
+
+}
