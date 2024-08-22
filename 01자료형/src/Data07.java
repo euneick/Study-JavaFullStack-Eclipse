9c811aac166235b6d0fc9f090871ea3ba9cf8c1a
euneick <study.euneick@gmail.com>
2024-08-22 13:19:34 +0900

    문자 형태의 자료형 저장하기

---
+++ 01자료형/src/Data07.java
@@ -0,0 +1,17 @@
+
+/**
+ * 문자 형태의 자료형 저장하기
+ */
+public class Data07 {
+
+	public static void main(String[] args) {
+
+		char c;
+
+		c = 'A';
+
+		System.out.println(c);
+		System.out.printf("%d %c", (int) c, c);
+	}
+
+}
