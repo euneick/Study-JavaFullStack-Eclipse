8624d4c23bced5df15574ac1fa24fa650c4e7e1b
euneick <study.euneick@gmail.com>
2024-08-22 10:55:36 +0900

    정수형 상수를 저장하는 변수 메모리 만들기

---
+++ 01자료형/src/Data03.java
@@ -0,0 +1,19 @@
+
+/**
+ * 정수형 상수를 저장하는 변수 메모리 만들기
+ */
+public class Data03 {
+
+	public static void main(String[] args) {
+
+		int a;
+		a = 1;
+		
+		System.out.println(a);
+		
+		a = 2;
+		
+		System.out.println(a);
+	}
+
+}
