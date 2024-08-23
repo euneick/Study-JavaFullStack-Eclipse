6299e38de33573dcad7def8b2690ce07ab1d0ad0
euneick <study.euneick@gmail.com>
2024-08-23 10:40:28 +0900

    조건연산자를 이용해서 최대값 구하기

---
+++ 02연산자/src/Opr03.java
@@ -0,0 +1,17 @@
+
+/**
+ * 조건연산자를 이용해서 최대값 구하기
+ */
+public class Opr03 {
+
+	public static void main(String[] args) {
+
+		int a = 5, b = 10;
+		int max;
+		
+		max = a > b ? a : b;
+		
+		System.out.println("최대값 : " + max);
+	}
+
+}
