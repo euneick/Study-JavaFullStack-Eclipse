fdf2ea2ecb8aa56399aa3796020a248b11c64ff0
euneick <study.euneick@gmail.com>
2024-08-23 12:41:46 +0900

    변수의 값을 1증가 시키는 증가연산자 ++

---
+++ 02연산자/src/Opr07.java
@@ -0,0 +1,25 @@
+
+/**
+ * 변수의 값을 1증가 시키는 증가연산자 ++
+ */
+public class Opr07 {
+
+	public static void main(String[] args) {
+
+		int a = 10, b = 10;
+
+		++a;
+		System.out.println(a);
+
+		b++;
+		System.out.println(b);
+
+		int c = 1;
+		System.out.println(c++);
+		System.out.println(c);
+		
+		System.out.println(++c);
+		System.out.println(c);
+	}
+
+}
