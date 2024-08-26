7ba19ea236a369879b376d0cd67592f13c327519
euneick <study.euneick@gmail.com>
2024-08-26 17:17:22 +0900

    do-while 반복문을 이용하여 1부터 5까지 반복해서 출력하는 예제

---
+++ 04반복문/src/While03.java
@@ -0,0 +1,15 @@
+
+/**
+ * do-while 반복문을 이용하여 1부터 5까지 반복해서 출력하는 예제
+ */
+public class While03 {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		do {
+			System.out.println("i : " + i++);
+		} while (i <= 5);
+	}
+
+}
