65f53546bdbc8a47614a4689bdaf07974b3911ed
euneick <study.euneick@gmail.com>
2024-08-26 16:39:17 +0900

    1 부터 5 까지의 합 구하기

---
+++ 04반복문/src/For04.java
@@ -0,0 +1,18 @@
+
+/**
+ * 1 부터 5 까지의 합 구하기
+ */
+public class For04 {
+
+	public static void main(String[] args) {
+
+		int i;
+		int total = 0;
+
+		for (i = 1; i <= 5; i++)
+			total += i;
+
+		System.out.println("total : " + total);
+	}
+
+}
