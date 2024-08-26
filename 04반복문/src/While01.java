5bc625711ac519c27a1393c2675b517765df4f89
euneick <study.euneick@gmail.com>
2024-08-26 16:50:38 +0900

    1 부터 5 사이의 자연수 반복해서 출력하기

---
+++ 04반복문/src/While01.java
@@ -0,0 +1,18 @@
+
+/**
+ * 1 부터 5 사이의 자연수 반복해서 출력하기
+ */
+public class While01 {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		
+		while (i <= 5) {
+			System.out.println("i : " + i);
+			
+			i++;
+		}
+	}
+
+}
