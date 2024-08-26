7f2ab10d072e36cf400cb9f06c43dc4ca17e9257
euneick <study.euneick@gmail.com>
2024-08-26 16:35:50 +0900

    구구단 2단 출력하기

---
+++ 04반복문/src/For03.java
@@ -0,0 +1,18 @@
+
+/**
+ * 구구단 2단 출력하기
+ */
+public class For03 {
+
+	public static void main(String[] args) {
+
+		int i;
+		int a = 2;
+
+		System.out.println("<<---" + a + "단--->>");
+		for (i = 1; i < 10; i++) {
+			System.out.println(a + " x " + i + " = " + (a * i));
+		}
+	}
+
+}
