92c3833ed59f959d2a797e725d3478650d3146d5
euneick <study.euneick@gmail.com>
2024-08-26 16:04:09 +0900

    제어 변수 값 반복해서 출력

---
+++ 04반복문/src/For01.java
@@ -0,0 +1,16 @@
+
+/**
+ * 제어 변수 값 반복해서 출력
+ */
+public class For01 {
+
+	public static void main(String[] args) {
+
+		int i;
+		for (i = 1; i <= 4; i++) {
+			System.out.println("i : " + i);
+		}
+		System.out.println("--------\nexited i : " + i);
+	}
+
+}
