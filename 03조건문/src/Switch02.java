534d5e657e7316a29c79bdf4b2eb70fe3f2d6218
euneick <study.euneick@gmail.com>
2024-08-26 15:47:36 +0900

    switch 문을 사용하여 학생 점수를 학점으로 출력하는 예제

--- 03조건문/src/Switch02.java
+++ 03조건문/src/Switch02.java
@@ -12,6 +12,12 @@ public class Switch02 {
 
 		System.out.print("학생 점수 입력 : ");
 		int score = scanner.nextInt();
+		
+		if (score < 0 || score > 100) {
+			System.out.println("잘못된 점수입니다.\n0에서 100사이의 정수를 입력하세요.");
+			return;
+		}
+		
 		String grade;
 
 		switch (score / 10) {
