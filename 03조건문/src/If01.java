33c4d91e3a358fdc5b2f3a6eb1f00ce952d8130c
euneick <study.euneick@gmail.com>
2024-08-26 14:45:32 +0900

    절대값 구하기

---
+++ 03조건문/src/If01.java
@@ -0,0 +1,15 @@
+
+/**
+ * 절대값 구하기
+ */
+public class If01 {
+
+	public static void main(String[] args) {
+
+		int num = -5;
+
+		if (num < 0) num *= -1;
+		System.out.println("absolute num = " + num);
+	}
+
+}
