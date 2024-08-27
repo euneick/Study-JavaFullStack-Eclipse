944554c95c5eda4d8c2d308542c382435c8df25c
euneick <study.euneick@gmail.com>
2024-08-27 18:11:19 +0900

    배열 연습문제 작성

---
+++ 07배열/src/test5.java
@@ -0,0 +1,20 @@
+
+/*
+ 문제. 다음은 배열을 선언하거나 초기화 한것이다.
+      잘못된 것을 고르고 그 이유를 작성 하시오. 4 5
+      
+      1. int[] arr[];
+      2. int[] arr = {1,2,3,}; 
+      3. int[] arr = new int[5];
+      4. int[] arr = new int[5]{1,2,3,4,5};
+      5. int arr[5];
+      6. int[] arr[] = new int[3][];
+ */
+public class test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 07배열/src/test6.java
@@ -0,0 +1,23 @@
+
+/*
+문제.  다음과 같은 배열이 있을 때, 
+      arr[3].length의 값은 얼마인가? 2
+
+	
+	int[][] arr = {
+					{5, 5, 5, 5, 5},
+					{10,10,10},
+					{20,20,20,20},
+					{30,30}
+				  };
+
+*/
+
+public class test6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
