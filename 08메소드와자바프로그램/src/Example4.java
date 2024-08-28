bce6c6fbbac337fdeafb584e4efd91d4797c99d2
euneick <study.euneick@gmail.com>
2024-08-28 10:41:49 +0900

    여러 매개변수를 가진 메소드

--- 08메소드와자바프로그램/src/Example4.java
+++ 08메소드와자바프로그램/src/Example4.java
@@ -9,9 +9,21 @@ public class Example4 {
 		System.out.println("이름 : " + name + ", 나이 : " + age);
 	}
 
+	public static int add(int n1, int n2) {
+
+		return n1 + n2;
+	}
+
+	public static float add(float f1, float f2) {
+
+		return f1 + f2;
+	}
+
 	public static void main(String[] args) {
 
 		printStudentInfo("홍길동", 30);
+		System.out.println(add(5, 10));
+		System.out.println(add(5.5f, 10.5f));
 	}
 
 }
