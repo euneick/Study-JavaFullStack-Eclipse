5dfb5a266d4137b1d36b0afdb7b42f5c35c07e33
euneick <study.euneick@gmail.com>
2024-08-28 09:56:57 +0900

    기본적인 메소드 예제

---
+++ 08메소드와자바프로그램/src/Example1.java
@@ -0,0 +1,18 @@
+
+/**
+ * 기본적인 메소드 예제
+ */
+public class Example1 {
+
+	public static int add(int a, int b) {
+
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+
+		int sum = add(5, 10);
+		System.out.println(sum);
+	}
+
+}
