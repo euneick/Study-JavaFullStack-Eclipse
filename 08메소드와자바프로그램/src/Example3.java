a03c827ecbc1b15fa4910122ec7108008b88cd6f
euneick <study.euneick@gmail.com>
2024-08-28 10:15:18 +0900

    매개변수가 없는 메소드

---
+++ 08메소드와자바프로그램/src/Example3.java
@@ -0,0 +1,17 @@
+
+/**
+ * 매개변수가 없는 메소드
+ */
+public class Example3 {
+
+	public static void greet() {
+		System.out.println("Hello World!");
+	}
+	
+	public static void main(String[] args) {
+		
+		greet();
+		Example3.greet();
+	}
+
+}
