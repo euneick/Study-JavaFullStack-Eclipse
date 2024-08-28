1ccc65a16982e8d54ad237fa240297cb7aaf6294
euneick <study.euneick@gmail.com>
2024-08-28 10:07:16 +0900

    반환값이 없는 메소드

---
+++ 08메소드와자바프로그램/src/Example2.java
@@ -0,0 +1,16 @@
+
+/**
+ * 반환값이 없는 메소드
+ */
+public class Example2 {
+
+	public static void printMessage(String message) {
+		System.out.println(message);
+	}
+
+	public static void main(String[] args) {
+
+		printMessage("hello world!");
+	}
+
+}
