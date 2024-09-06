ff34a6efa88fefdce298232de5fd2282b1d2b399
euneick <study.euneick@gmail.com>
2024-09-06 16:58:19 +0900

    메소드 오버라이딩, 업캐스팅 활용 예제

---
+++ 17메소드오버라이딩업캐스팅응용/src/Test2.java
@@ -0,0 +1,66 @@
+
+/**
+ * 메소드 오버라이딩, 업캐스팅 활용 예제
+ */
+class 해장국 {
+
+	public void 간맞추기() {
+
+		System.out.println("소금치기");
+	}
+}
+
+class 뼈다귀해장국 extends 해장국 {
+
+	@Override
+	public void 간맞추기() {
+
+		System.out.println("들깨가루 넣기");
+	}
+
+	public void 뼈추가하기() {
+
+		System.out.println("뼈 추가");
+	}
+}
+
+class 콩나물해장국 extends 해장국 {
+
+	@Override
+	public void 간맞추기() {
+
+		System.out.println("고춧가루 넣기");
+	}
+
+	public void 콩나물넣기() {
+
+		System.out.println("콩나물 넣기");
+	}
+}
+
+class 취객 {
+
+	public void 해장국간맞추기(해장국 해장국) {
+
+		해장국.간맞추기();
+	}
+}
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		취객 취객1 = new 취객();
+
+		뼈다귀해장국 뼈다귀해장국 = new 뼈다귀해장국();
+		
+		취객1.해장국간맞추기(뼈다귀해장국);
+		뼈다귀해장국.뼈추가하기();
+
+		콩나물해장국 콩나물해장국 = new 콩나물해장국();
+		
+		취객1.해장국간맞추기(콩나물해장국);
+		콩나물해장국.콩나물넣기();
+	}
+
+}
