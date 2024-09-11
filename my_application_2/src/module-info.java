7fbdc53bfb0b2151eeabd802b64d16687bb6fcf7
euneick <study.euneick@gmail.com>
2024-09-11 12:52:16 +0900

    전의 의존 예제

--- my_application_2/src/module-info.java
+++ my_application_2/src/module-info.java
@@ -6,5 +6,5 @@ module my_application_2 {
 	 * requires 키워드를 통해 의존 설정
 	 */
 	requires my_module_a;
-	requires my_module_b;
+//	requires my_module_b;		// my_module_a 에서 my_module_b를 전의 의존을 하는지 확인을 위해 주석 처리
 }
--- my_module_a/src/module-info.java
+++ my_module_a/src/module-info.java
@@ -3,4 +3,7 @@ module my_module_a {
 	// 특정 패지키를 외부 프로젝트에서 사용 할 수 있도록 exports 키워드를 작성
 	exports pack1;
 //	exports pack2;
+	
+	// requires transitive 키워드를 사용해 전의 의존 설정
+	requires transitive  my_module_b;
 }
