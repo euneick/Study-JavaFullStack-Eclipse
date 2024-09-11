8bf6e4de54a33d8f040c6e6406e42e17c77c3895
euneick <study.euneick@gmail.com>
2024-09-11 11:41:47 +0900

    모듈 생성 및 적용 방법 예제

---
+++ my_application_2/src/module-info.java
@@ -0,0 +1,10 @@
+module my_application_2 {
+	
+	/**
+	 * build path - configure build path - project - Modulepath 에서
+	 * 사용 할 모듈이 있는 프로젝트를 add 하고 적용
+	 * requires 키워드를 통해 의존 설정
+	 */
+	requires my_module_a;
+	requires my_module_b;
+}
---
+++ my_module_b/src/module-info.java
@@ -0,0 +1,6 @@
+module my_module_b {
+
+	// 특정 패지키를 외부 프로젝트에서 사용 할 수 있도록 exports 키워드를 작성
+	exports pack3;
+	exports pack4;
+}
