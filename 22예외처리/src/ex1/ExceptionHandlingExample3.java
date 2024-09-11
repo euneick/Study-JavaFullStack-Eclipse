07130646faa5b98c83ab0c0a43a5e9c6c265d388
euneick <study.euneick@gmail.com>
2024-09-11 16:56:18 +0900

    예외 처리 예제

---
+++ 22예외처리/src/ex1/ExceptionHandlingExample3.java
@@ -0,0 +1,34 @@
+
+ /**
+  * 예외 처리 예제
+  */
+package ex1;
+
+public class ExceptionHandlingExample3 {
+
+	public static void main(String[] args) {
+
+		try {
+			
+			Class.forName("java.lang.String");
+			System.out.println("java.lang.String 클래스가 존재합니다.");
+		}
+		catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		System.out.println();
+
+		try {
+
+			Class.forName("java.lang.String2");
+			System.out.println("java.lang.String2 클래스가 존재합니다.");
+		}
+		catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+}
