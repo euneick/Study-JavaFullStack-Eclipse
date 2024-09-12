3d1e9941843d4d5cb52d129d82a52e4639187bb1
euneick <study.euneick@gmail.com>
2024-09-12 10:51:24 +0900

    예외 처리 전달하기 예제

---
+++ 22예외처리/src/ex2/ThrowsExample1.java
@@ -0,0 +1,23 @@
+
+/**
+ * 예외 처리 전달하기 예제
+ */
+package ex2;
+
+public class ThrowsExample1 {
+
+	public static void main(String[] args) {
+
+		try {
+			findClass();
+		} catch (ClassNotFoundException e) {
+			System.out.println("예외 처리 : " + e.toString());
+		}
+	}
+
+	public static void findClass() throws ClassNotFoundException {
+
+		Class.forName("java.lang.String2");
+	}
+
+}
