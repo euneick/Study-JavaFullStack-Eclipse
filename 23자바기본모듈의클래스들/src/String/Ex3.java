5addc5ccfd0208af4ff97a669e949d9d879dac19
euneick <study.euneick@gmail.com>
2024-09-13 16:38:32 +0900

    StringBuffer 클래스 예제 및 StringBuilder 클래스 예제

--- 23자바기본모듈의클래스들/src/String/Ex3.java
+++ 23자바기본모듈의클래스들/src/String/Ex3.java
@@ -1,6 +1,7 @@
 
 /**
  * StringBuffer 클래스 예제
+ * StringBuilder 클래스 예제
  */
 package String;
 
@@ -8,7 +9,23 @@ public class Ex3 {
 
 	public static void main(String[] args) {
 		
+		StringBuffer stringBuffer = new StringBuffer("JAVA");
+
+		System.out.println(stringBuffer);
 		
+		stringBuffer.append(" programming");
+		
+		System.out.println(stringBuffer);
+		
+		System.out.println("===========================================");
+		
+		String data = new StringBuilder()
+				.append("DEF")
+				.insert(0, "ABC")
+				.delete(3, 4)
+				.toString();
+		
+		System.out.println(data);
 	}
 
 }
