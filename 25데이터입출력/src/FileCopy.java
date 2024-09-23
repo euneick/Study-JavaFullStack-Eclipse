5b4f9b977f8185c1090ad52e36233d5f11f1b51b
euneick <study.euneick@gmail.com>
2024-09-23 17:16:37 +0900

    File Input/Output Stream 객체를 활용하여 파일 복사 프로그램 예제

---
+++ 25데이터입출력/src/FileCopy.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+/**
+ * File Input/Output Stream 객체를 활용하여 파일 복사 프로그램 예제
+ */
+public class FileCopy {
+
+	public static void main(String[] args) throws Exception {
+		
+		String path = "C:\\ProjectPlace\\JavaFullStack\\a\\";
+		String originFileName;
+		String copiedFileName;
+		
+		int data = 0;
+		int size = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("복사할 파일 이름 : ");
+		originFileName = scanner.next();
+		System.out.print("복사 생성 할 파일 이름 : ");
+		copiedFileName = scanner.next();
+		
+		scanner.close();
+		
+		File file = null;
+		FileInputStream fis = new FileInputStream(path + originFileName);
+		FileOutputStream fos = new FileOutputStream(path + copiedFileName);
+		
+		while((data = fis.read()) != -1) {
+			fos.write(data);
+			
+			size++;
+		}
+	}
+
+}
