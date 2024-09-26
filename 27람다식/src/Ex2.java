623bf8d9eeb387f2698902c519b91158d146d525
euneick <study.euneick@gmail.com>
2024-09-26 10:34:55 +0900

    람다식 예제

---
+++ 27람다식/src/Ex2.java
@@ -0,0 +1,24 @@
+import java.util.function.Predicate;
+
+/**
+ * 람다식 예제
+ * 		(매개변수) -> { 기능 구현; }  
+ * 
+ * 함수형 인터페이스 Predicate<T>
+ * 
+ * 		T : 입력 타입
+ * 		반환타입은 Boolean
+ * 
+ * 		test() 메소드를 통해 익명객체의 리턴값을 받을 수 있음
+ */
+public class Ex2 {
+
+	public static void main(String[] args) {
+
+		Predicate<String> isEmpty = s -> s.isEmpty();
+		
+		System.out.println(isEmpty.test("hello"));
+		System.out.println(isEmpty.test(new String()));
+	}
+
+}
